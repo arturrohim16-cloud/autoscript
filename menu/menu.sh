dd7dfc1bd82a3eb02bfa578e0051e4eb07aaab22
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-21 20:13:43 +0700

    Fix formatting in menu options display

--- menu/menu.sh
+++ menu/menu.sh
@@ -145,12 +145,12 @@ echo -e "$pembatas"
 
 # Bagian Menu (Dua Kolom - Spasi sudah diatur agar lurus)
 echo -e "   $tengah${WHITE}[1]${NC} ${GREEN}MENU SSH${NC}          ${WHITE}[7]${NC} ${GREEN}BOT TELEGRAM       $tengah"
-echo -e "   $tengah${WHITE}[2]${NC} ${GREEN}MENU VMESS             ${WHITE}[8]${NC} ${GREEN}UPDATE SCRIPT      $tengah"
-echo -e "   $tengah${WHITE}[3]${NC} ${GREEN}MENU TROJAN            ${WHITE}[9]${NC} ${GREEN}MENU SYSTEM        $tengah"
-echo -e "   $tengah${WHITE}[4]${NC} ${GREEN}MENU VLESS             ${WHITE}[10]${NC} ${GREEN}CEK RUNNING       $tengah"
-echo -e "   $tengah${WHITE}[5]${NC} ${GREEN}MENU SUHODKES          ${WHITE}[11]${NC} ${GREEN}CELAR RAM         $tengah"
-echo -e "   $tengah${WHITE}[6]${NC} ${GREEN}INSTAL UDP             ${WHITE} [x]${NC} ${GREEN}EXIT              $tengah"
-echo -e "   $pembatas"
+echo -e "   $tengah${WHITE}[2]${NC} ${GREEN}MENU VMESS{NC}         ${WHITE}[8]${NC} ${GREEN}UPDATE SCRIPT      $tengah"
+echo -e "   $tengah${WHITE}[3]${NC} ${GREEN}MENU TROJAN{NC}        ${WHITE}[9]${NC} ${GREEN}MENU SYSTEM        $tengah"
+echo -e "   $tengah${WHITE}[4]${NC} ${GREEN}MENU VLESS{NC}         ${WHITE}[10]${NC} ${GREEN}CEK RUNNING       $tengah"
+echo -e "   $tengah${WHITE}[5]${NC} ${GREEN}MENU SUHODKES{NC}      ${WHITE}[11]${NC} ${GREEN}CELAR RAM         $tengah"
+echo -e "   $tengah${WHITE}[6]${NC} ${GREEN}INSTAL UDP{NC}         ${WHITE} [x]${NC} ${GREEN}EXIT              $tengah"
+echo -e "$pembatas"
 
 # Bagian Client
 echo -e "$tengah ${GREEN} Client Name ${NC}: $Name "
