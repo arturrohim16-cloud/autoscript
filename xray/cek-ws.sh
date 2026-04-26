e02c48a0c85dae06da3ee066c6a8f44f3a5b03f3
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-26 15:30:35 +0700

    Change grep pattern to fetch Vmess user logins

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -22,7 +22,7 @@ exit 0
 fi
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^#vmsg' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vmess User Login ]=---------";
 echo "----------------------------------------";
