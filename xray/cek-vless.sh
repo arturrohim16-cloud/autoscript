99a9598c68f25c3d1b93c92b1b0010eeb7b3a3c0
Ajijainalmaster <arturrohim16@gmail.com>
2026-04-26 15:28:46 +0700

    Change data extraction pattern in cek-vless.sh

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -22,7 +22,7 @@ exit 0
 fi
 clear
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
+data=( `cat /etc/xray/config.json | grep '^#vlsg' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
 echo "---------=[ Vless User Login ]=---------";
 echo "----------------------------------------";
