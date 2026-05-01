6988f663458bb5fc31d96fbc2d0b17a483b00493
Ajijainalmaster <arturrohim16@gmail.com>
2026-05-02 01:01:07 +0700

    Fix formatting in m-bot-notif.sh script

--- menu/m-bot-notif.sh
+++ menu/m-bot-notif.sh
@@ -145,7 +145,7 @@ case $opsi in
     ;;
 2)
     clear
-    sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update -y && apt install -y git && apt install -y curl && curl -L -k -sS https://raw.githubusercontent.com/arivpnstores/BotVPN2/main/start -o start && bash start sellvpn && [ $? -eq 0 ] && rm -f start
+    sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update -y && apt install -y git && apt install -y curl && curl -L -k -sS https://raw.githubusercontent.com/arivpnstores/BotVPN/main/start -o start && bash start sellvpn && [ $? -eq 0 ] && rm -f start
     baris_panjang
     Sc_Credit 
     ;;
