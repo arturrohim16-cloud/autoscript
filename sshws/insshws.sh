c0401abdffebe1c39f51d97e2ada79b917683e3f
Ajijainalmaster <arturrohim16@gmail.com>
2026-05-03 00:42:22 +0700

    Enhance insshws.sh to include WebSocket setup
    Added installation and service management for WebSocket components.

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -1,18 +1,39 @@
-#!/bin/bash
-clear
 cd
+apt install python -y
+apt install python3 -y
 
-#Install Script Websocket-SSH golang
-wget -O /usr/local/bin/ws-stunnel.go https://autoscript.caliphdev.com/sshws/ws-stunnel.go
-/usr/local/go/bin/go build -o /usr/local/bin/ws-stunnel /usr/local/bin/ws-stunnel.go
-chmod +x /usr/local/bin/ws-stunnel
-wget -O /etc/systemd/system/ws-stunnel.service https://autoscript.caliphdev.com/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /usr/local/bin/WebSocket https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.py
+wget -O /usr/local/bin/WebSocket.SSH https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.SSH.py
+wget -O /usr/local/bin/WebSocket.OVPN https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.OVPN.py
+
+chmod +x /usr/local/bin/WebSocket
+chmod +x /usr/local/bin/WebSocket.SSH
+chmod +x /usr/local/bin/WebSocket.OVPN
 
 
-#restart service
+wget -O /etc/systemd/system/WebSocket.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.service && chmod +x /etc/systemd/system/WebSocket.service
+
+wget -O /etc/systemd/system/WebSocket.SSH.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.SSH.service && chmod +x /etc/systemd/system/WebSocket.SSH.service
+
+wget -O /etc/systemd/system/WebSocket.OVPN.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.OVPN.service && chmod +x /etc/systemd/system/WebSocket.OVPN.service
+
+
 systemctl daemon-reload
 
-#Enable & Start & Restart ws-openssh service
-systemctl enable ws-stunnel.service
-systemctl start ws-stunnel.service
-systemctl restart ws-stunnel.service
+systemctl enable WebSocket.service
+systemctl start WebSocket.service
+systemctl restart WebSocket.service
+
+systemctl enable WebSocket.SSH.service
+systemctl start WebSocket.SSH.service
+systemctl restart WebSocket.SSH.service
+
+systemctl enable WebSocket.OVPN.service
+systemctl start WebSocket.OVPN.service
+systemctl restart WebSocket.OVPN.service
+
+mkdir /etc/ws > /dev/null 2>&1
+rm /etc/ws/status
+rm /etc/ws/status2
+echo 'SSH ' >> /etc/ws/status
+echo 'SSH' >> /etc/ws/status2
